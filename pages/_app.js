d3f88145daeddf46ca45639e7bd09c1c100a5be4
kosma-kurlowicz <kosma.kurlowicz@gmail.com>
2021-07-14 22:10:01 +0200

    Adds modified keys in app component.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
   return <Component {...pageProps} key={router.asPath} />
 }
 
